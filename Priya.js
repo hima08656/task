79b39dd0127d2e7cb2e3e94cd323993bddb8806d
Hima Krishna Priy <yadlahimakrishnapriya@gmail.com>
2025-11-27 11:28:15 +0530

    Task

---
+++ Priya.js
@@ -0,0 +1,10 @@
+// LO - Understanding Synchronous Code Execution
+
+console.log("Task 1");
+console.log("Task 2");
+console.log("Task 3");
+
+// Expected output (in order):
+// Task 1
+// Task 2
+// Task 3
